3d4f72b05f5d40b3630f62a96f18eec5b8343d1b
Jeff <jeff.manmaru@gmail.com>
2023-03-08 21:50:52 -0800

    Add some Code Comments and README info

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# master-duff-bot
-Code that powers the "Master Duff" bot in the Mario Golf Super Rush Discord server
+# Master Duff Bot
+Code that powers the "Master Duff" bot in the Mario Golf Super Rush (MGSR) Discord server
+
+### Purposes
+* Currently Master Duff is being used to calculate some basic and advanced player stats for people who play MGSR competitively in our Elo-based Ranked Ladder
+* The Elo Ladder is powered by TeamUp Bot, the stats are calcuated by scraping the channel where we report the results of those matches and parsing the standardized text that the TeamUp Bot outputs
+* These rankings and metrics help us decide seeding for in-server tournaments like March Madness as well as our upcoming 2nd League Season
+
+### Going Forward
+* The bot is not very interactive at the moment, but when time permits I intend to enable basic Bot-like functionality such as a command akin to "Hi Master Duff, what is my Max Elo ever attained?"
+* Right now the "All-Time" stats only go as far back as Jan 20th, 2022 because the current dedictaed `elo-recording` channel was not established until then.  However, the Elo Ladder was actually first started in Dec 2021 but original the reports/records were scattered in a variety of channels.  If time permits, I'd like to add code to collect those early reports as well.
+* Hitting the Discord API every time we run the Bot is wasteful, start with caching the lookups locally (maybe up to last 1-2 weeks since old records are never edited).  For now a local file will suffice, in the long-run, if time and funds permit, set up an actual quick-access tabular database for easy queries.
+
+### Thanks
+* Mrs Chippy, Founder of the MGSR Server Elo Ladder
+* TeamUp Bot and its Creator
--- master_duff.py
+++ master_duff.py
@@ -4,21 +4,16 @@ from datetime import datetime, timezone
 import discord
 import json
 
-RECORDING_CHANNEL = 933895315373838416
-TEAMUP_SUPPORT_MSG = "Visit the Team Up Discord Support Server"
-RECORD_UNDO_MSG = "Result Removed"
-
+# Script Config
 SHOW_OPPONENT_STATS_FOR = None
 
-TSV_LINE = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}"
-PER_PLAYER_TSV_LINE = "{}\t{}\t{}\t{}\t{}\t{}"
+# Channel IDs
+RECORDING_CHANNEL = 933895315373838416
 
-ELITE_LEVEL_ELO = 1500
-PRO_LEVEL_ELO = 1400
-
-ONE_MONTH_AGO = datetime.now(timezone.utc) + relativedelta(months = -1)
-THREE_MONTHS_AGO = datetime.now(timezone.utc) + relativedelta(months = -3)
-
+# Player Historical Account Name Changes
+## If a player changes their nickname, they do not need listed here
+## However if they change their actual Discord account name, they need
+## added here so that the pre/post account name change stats map to same person
 ACCOUNT_ALIAS_LOOKUP = {
     "Blue Toad": "BT",
     "sdreb3421": "Dr. Ebick",
@@ -47,6 +42,21 @@ ACCOUNT_ALIAS_LOOKUP = {
     "\\u0561\\u0268\\u057c\\u0236\\u025b\\u0280": "winter"
 }
 
+# Strings that appear in Non-Success Recording cases
+TEAMUP_SUPPORT_MSG = "Visit the Team Up Discord Support Server"
+RECORD_UNDO_MSG = "Result Removed"
+
+# Output skeletons
+TSV_LINE = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}"
+PER_PLAYER_TSV_LINE = "{}\t{}\t{}\t{}\t{}\t{}"
+
+# Stat Calculation Helpers
+ELITE_LEVEL_ELO = 1500
+PRO_LEVEL_ELO = 1400
+ONE_MONTH_AGO = datetime.now(timezone.utc) + relativedelta(months = -1)
+THREE_MONTHS_AGO = datetime.now(timezone.utc) + relativedelta(months = -3)
+
+# Bot setup, allow it to read message content from channel
 bot_intents = discord.Intents.default()
 bot_intents.message_content = True
 client = discord.Client(intents=bot_intents)
@@ -54,10 +64,13 @@ client = discord.Client(intents=bot_intents)
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+    # Calculuate and output stats on right after boot-up
     await produceStats()
 
 @client.event
 async def on_message(message):
+    # TODO: Add actual bot interactivity someday
     if message.author.display_name != "manmaru":
         return
 
@@ -65,13 +78,25 @@ async def on_message(message):
         await message.channel.send('Yes, indeed I am')
 
 async def produceStats():
+    # Get Discord channel object
     elo_channel = client.get_channel(RECORDING_CHANNEL)
-    elo_stats = {}
+
+    # Init "stack" variable to track how many records need dropped due to undo
     need_to_undo_count = 0
 
+    elo_stats = {}
+
+    # The below method iterates on every message in the channel
+    ## "limit = None" means entire history of messages
+    ## if limit is specified it will be most recent {limit} messages
+    ## This is the main Discord API method we use to fetch message history
     print("Fetching messages...")
     async for message in elo_channel.history(limit = None):
+
+        # The text of TeamUp Elo reports comes in as an "embed" in a message, not raw text
+        # That is why in Discord the text looks slightly indented with non-standard formatting
         if len(message.embeds) == 0:
+            # If it's a plain message (no embeds), it is not an Elo report -- ignore it
             continue
 
         elo_info = message.embeds[0].to_dict()
@@ -79,6 +104,8 @@ async def produceStats():
             continue
 
         try:
+            # Title of first embed
+            # Will be "Game Recorded: " for the Elo reports we are seeking
             first_item_name = elo_info["fields"][0]["name"]
         except Exception:
             first_item_name = ""
@@ -87,6 +114,8 @@ async def produceStats():
             continue
 
         if first_item_name == RECORD_UNDO_MSG:
+            # Bump the stack indicating subsequent report should be undone
+            # Next actual Elo report will be ignored if this is greater than zero
             need_to_undo_count += 1
             continue
 
@@ -95,15 +124,22 @@ async def produceStats():
             need_to_undo_count -= 1
             continue
 
-        time_stamp = message.created_at
+        # TODO This is the right spot to write to the lookup cache
+        # Will need to end the channel-fetch loop here, and do a second loop for evaluateRecord
+
+        # Process raw strings from Elo report into structured player results
         record_stats = evaluateRecord(elo_info["fields"])
         if record_stats is None:
             continue
 
+        time_stamp = message.created_at
         for player_account in record_stats.keys():
+
+            # Add the individual W/L/T and Elo info from a single report to the overall stats dataset
             record = record_stats[player_account]
             before_elo = record["before_elo"]
             if player_account not in elo_stats.keys():
+                # Add new blank record in overall Elo dataset if this player name hasn't appeared yet
                 elo_stats[player_account] = {
                     "rounds": 0,
                     "wins": [],
@@ -122,6 +158,7 @@ async def produceStats():
                     "cleared_1600_date": None,
                     "cleared_1700_date": None,
                     "first_round": time_stamp,
+                    # Since message are scanned in time-descending order, this value will never need updated
                     "last_round": time_stamp,
                 }
 
@@ -130,12 +167,12 @@ async def produceStats():
             elo_stats[player_account]["losses"].extend(record["losses"])
             elo_stats[player_account]["ties"].extend(record["ties"])
 
-            # Messages are scanned in descending order
+            # Continually update "first_round" with current time since record messages are scanned in time-descending order
             elo_stats[player_account]["first_round"] = time_stamp
 
             after_elo = record["after_elo"]
 
-            # Update 1month min/max Elo
+            # Update 1-month min/max Elo
             if time_stamp >= ONE_MONTH_AGO:
                 if before_elo > elo_stats[player_account]["max_elo_1m"]:
                     elo_stats[player_account]["max_elo_1m"] = before_elo
@@ -147,7 +184,7 @@ async def produceStats():
                 if after_elo < elo_stats[player_account]["min_elo_1m"]:
                     elo_stats[player_account]["min_elo_1m"] = after_elo
 
-            # Update 3month min/max Elo
+            # Update 3-month min/max Elo
             if time_stamp >= THREE_MONTHS_AGO:
                 if before_elo > elo_stats[player_account]["max_elo_3m"]:
                     elo_stats[player_account]["max_elo_3m"] = before_elo
@@ -159,7 +196,7 @@ async def produceStats():
                 if after_elo < elo_stats[player_account]["min_elo_3m"]:
                     elo_stats[player_account]["min_elo_3m"] = after_elo
 
-            # Update min/max all-time Elo
+            # Update All-Time min/max Elo
             if before_elo > elo_stats[player_account]["max_elo_full"]:
                 elo_stats[player_account]["max_elo_full"] = before_elo
                 elo_stats[player_account]["max_elo_date"] = time_stamp
@@ -210,17 +247,20 @@ async def produceStats():
             )
 
     print("")
+
+    # Add in more advanced stats, such as win rate against elite players
     full_stats = calculuateSupplementalStats(elo_stats)
 
     if SHOW_OPPONENT_STATS_FOR is not None and SHOW_OPPONENT_STATS_FOR in full_stats:
+        # Script should output the per-oppoennt W/L/T stats for a single specific player
         outputPlayerMatchupResults(SHOW_OPPONENT_STATS_FOR, full_stats[SHOW_OPPONENT_STATS_FOR])
     else:
+        # Script should output stats for all players
         outputFullStats(full_stats)
 
 def calculuateSupplementalStats(elo_stats):
+    # Get the list of players who will count as "elite" in the advanced stats
     elite_players = getPlayersByMaxEloCutoff(elo_stats, ELITE_LEVEL_ELO)
-    print(json.dumps(elite_players))
-    print("")
 
     all_players = elo_stats.keys()
     for player in all_players:
@@ -228,6 +268,7 @@ def calculuateSupplementalStats(elo_stats):
         elo_stats[player]["elite_losses"] = 0
         elo_stats[player]["elite_ties"] = 0
 
+        # Add a W/L/T if the opponent in question has ever reached the Elite level Elo threshold
         for win_against_player in elo_stats[player]["wins"]:
             if win_against_player in elite_players:
                 elo_stats[player]["elite_wins"] += 1
@@ -243,6 +284,7 @@ def calculuateSupplementalStats(elo_stats):
     return elo_stats
 
 def outputFullStats(elo_stats):
+    # Column titles for spreadsheet-friendly TSV output
     print(TSV_LINE.format(
         "Player Account",
         "Total Rounds Played",
@@ -277,6 +319,7 @@ def outputFullStats(elo_stats):
     for player in sorted_players:
         player_stats = elo_stats[player]
 
+        # Most of this logic is to format the "elo_stats" dataset for friendly output into spreadsheet
         win_count = len(player_stats["wins"])
         loss_count = len(player_stats["losses"])
         tie_count = len(player_stats["ties"])
@@ -288,6 +331,8 @@ def outputFullStats(elo_stats):
         elite_matches = elite_win_count + elite_loss_count + elite_tie_count
         elite_match_rate = round(100.0 * elite_matches / total_matches, 1)
 
+        # The "min/max Elo" values won't be accurate for players who haven't played within the specified time period
+        # So, use this to just show a blank value for a player who has no records within last N months
         show_last_3m = player_stats["last_round"] >= THREE_MONTHS_AGO
         show_last_1m = player_stats["last_round"] >= ONE_MONTH_AGO
 
@@ -324,7 +369,6 @@ def outputFullStats(elo_stats):
     
 def outputPlayerMatchupResults(target_player, player_stats):
     results_per_opponent = {}
-
     for won_against_player in player_stats["wins"]:
         if not won_against_player in results_per_opponent:
             results_per_opponent[won_against_player] = initWinLossTieDict()
@@ -348,6 +392,7 @@ def outputPlayerMatchupResults(target_player, player_stats):
     print("Per Opponent Stats for MGSR Player: {}".format(target_player))
     print("")
 
+    # More simple spreadsheet-friendly TSV output format for per-opponent record stats
     print(PER_PLAYER_TSV_LINE.format(
         "Opponents",
         "Matches",
@@ -373,13 +418,16 @@ def outputPlayerMatchupResults(target_player, player_stats):
 
 
 def evaluateRecord(record_fields):
-    record_count = len(record_fields)
     parsed_records = []
     record_stats = {}
     try:
+        # There are one of these for each player that was in the report
+        # it contains the player account name, placement, and before/after Elo
         for single_record in record_fields:
             record_entry = {}
             value_parts = single_record["value"].split("\n")
+
+            # Sanitize the account name so we can correctly organize and attribute stats
             player_account = normalizeAccountName(value_parts[0].replace("***", ""))
             record_entry["player"] = player_account
 
@@ -398,9 +446,9 @@ def evaluateRecord(record_fields):
             after_elo = elo_parts[2].replace(")", "")
             record_entry["before_elo"] = int(before_elo)
             record_entry["after_elo"] = int(after_elo)
-
             parsed_records.append(record_entry)
 
+            # Init object to evaluate the W/L/T outcome of this record
             record_stats[player_account] = {
                 "placement": record_entry["placement"],
                 "before_elo": record_entry["before_elo"],
@@ -418,6 +466,8 @@ def evaluateRecord(record_fields):
 
                 record_placement = record["placement"]
                 record_player = record["player"]
+
+                # This can count both total W/L/T as well as per-opponent records
                 if player_placement < record_placement:
                     record_stats[player_account]["wins"].append(record_player)
                 elif player_placement == record_placement:
@@ -437,6 +487,9 @@ def normalizeAccountName(account_name):
     return account_name
 
 def getEloThresholdChange(before_elo, after_elo, elo_threshold, current_clear_date, record_timestamp):
+    # Part of the logic of this method is based on the fact that all record messages are scanned in time-descending order
+    # If that assumption was ever changed, this method would also need updated
+
     if before_elo < elo_threshold and after_elo >= elo_threshold:
         # A threshold is being crossed in this record
 
@@ -462,6 +515,7 @@ def initWinLossTieDict():
     }
 
 def renderWinPercent(win_count, tie_count, total_matches):
+    # Ensure 100% win perentage is rendered as "1.000", others as 3-digit numbers like ".xyz"
     if total_matches == 0:
         return ".000"
 
@@ -489,4 +543,6 @@ def getPlayersByMaxEloCutoff(stats_per_player, max_elo_cutoff):
             exceeding_players.append(player)
     return exceeding_players
 
+# This is the API security key which allows the MGSR server to identify this code as Master Duff
+# In GitHub, this is a fake value, use the real value in the environment where the Bot is actually run
 client.run('the-key')
